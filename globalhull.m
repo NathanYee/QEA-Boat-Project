e744902226869d7b81525fb95abb959d36be32dc
nathan <nathaniel.yee@students.olin.edu>
2016-02-08 09:49:44 -0500

    Finished a first pass matlab code globalhull.m

--- globalhull.m
+++ globalhull.m
@@ -1,11 +1,15 @@
 function z_position = globalhull(x,y,params)
-%% returns z_position
+%% globalhull returns z_position given a position in the xy plane and a variable paramemter
+
 %% compute value of z
-z = abs(y)^params - 1
+z = abs(y)^params - 1;
 
-%%
+%% if out of bounds, z = 0
+if z > 0
+    z = 0;
+end
 
+%% return z_position
 
-
-
+z_position = z;
 end
