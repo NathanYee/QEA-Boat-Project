9704085c7c4f6fce788ab9a25812a68167b3146a
charlievweiss <charlotte.weiss@students.olin.edu>
2016-02-17 09:59:43 -0500

    Waterline now outputs d, not a vector. Waterline2 is an old version of waterline.m BoatCalcBugZero is Paige's waterline code

--- firstPass/waterline.m
+++ firstPass/waterline.m
@@ -3,7 +3,7 @@
 % the problem lies in comparing vWater against vDis. Pretty sure the
 % integration is working properly.
 
-function Waterline = waterline(mBoat,theta,X)
+function depth = waterline(mBoat,theta)
 %% waterline will return a plottable vector of position values for the waterline by
 %the distance below y=0
 
@@ -11,61 +11,49 @@ function Waterline = waterline(mBoat,theta,X)
 pWater = 1; % g/cm^3
 vDis = mBoat/pWater; %volume water displaced (m^3)
 vWater = 0; %volume of water displaced given by d
-d = 0; %depth of waterline from hull
-bottomboat = -1; % bottom of boat
+d = -.9; %depth of waterline from hull
 %% Function for shape of boat
-funXZ = 1; %will use this later in 3d version
-funYZ = @(y) abs(y).^1 - 1;
+%funXZ = 1; %will use this later in 3d version
+funcYZ = @(y) abs(y).^1 - 1;
 %% Function for waterline based on d
-funWater = @(y) tan(theta)*y+d;
+funcWater = @(y,d) tan(theta)*y+d;
 %% check things for value of d
-step = bottomboat/100; %checks 100 times
-check = vDis;
+step = 1/100; %checks 100 times
+check = realmax;
 w = 0; % variable to hold correct value of d
-while d > bottomboat
+while d <= 0
     % Find intercepts
     %%
-%     step = -1/100;
-%     vDis = 1;
-%     check = vDis;
-%     theta = pi+.001;
-%     d = -.1;
-%     funYZ = @(y) abs(y).^1 - 1;
-%     funWater = @(y) tan(theta)*y+d;
-    hullfun = @(a) funYZ(a) - funWater(a); % waterline and boat hull
-    y0fun = @(b) funWater(b); % waterline and y axis
-    y1 = fzero(hullfun,-1);
-    y2 = fzero(y0fun,-1);
-    y3 = fzero(hullfun,1);
-    
+    hullfunc = @(a) funcYZ(a) - funcWater(a,d); % waterline and boat hull
+    y0func = @(b) funcWater(b,d); % waterline and y axis
+    y1 = fzero(hullfunc,-1);
+    if theta ~= pi && theta ~= 0
+        y2 = fzero(y0func,-1);
+    end
+    y3 = fzero(hullfunc,1);
+    keyboard
     % Find vWater for current d
-    doublefun = @(y,z) z./z;
-    if y1 ~= y3 && y2 > y3
-        % Case 1: waterline intercepts hull twice
-        vWater = integral2(doublefun,y1,y3,funYZ,funWater);
+    doublefunc = @(y,z) z./z; %some shit for integral2
+    if theta == pi || theta == 0
+        % Weird case #1: theta = pi
+        vWater = integral2(doublefunc,y1,y3,funcYZ,d); 
+    elseif y1 ~= y3 && y2 > y3
+        %Case 2: waterline intercepts hull twice
+        vWater = integral2(doublefunc,y1,y3,funcYZ,funcWater);
     else
-        % Case 2: waterline intercepts deck (y axis)
-        vWater = integral2(doublefun,y1,y2,funYZ,funWater);%integral(funYZ,y2,1);
+        %Case 1: waterline intercepts deck (y axis)
+        vWater = integral2(doublefunc,y1,y2,funcYZ,funcWater(d))-integral(funcYZ,y2,1);
     end
     
     % Compare vWater againts vDis
     if abs(vDis-vWater) <= check
-        lastvWater = vWater;
         w = d;
+        lastvwater = vWater;
         check = abs(vDis-vWater);
     end
     %%
     d = d + step;
 end
-
 %% Final result: matrix of z values for waterline
-W = zeros(1,length(X));
-for i=1:length(X)
-    W(i) = tan(theta)*X(i) + w;
-end
-Waterline = W;
-% some random shit it prints
-depth = w
-thingthatshouldbe0 = check
-lastvWater1 = lastvWater
+depth = w;
 end
