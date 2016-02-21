0a267b6272e02d5498f8c64996fadfa5f59443cf
charlievweiss <charlotte.weiss@students.olin.edu>
2016-02-21 13:39:27 -0500

    checking code for bugs

--- secondAttempt/avs.m
+++ secondAttempt/avs.m
@@ -1,6 +1,7 @@
 function res = avs(mBoat,n,z)
 %avs fzero's to find the avs.  This is the angle at which our boat becomes
 %unstable
+
 res = fzero(@(theta) rightingArm(mBoat,theta,n,z), 115); %returns angle
 
 end
